a5b6bfe601529cb3d463347e886d285aad5724ca
JC-ProPlus <guo121guo@gmail.com>
2020-04-15 16:12:42 +0800

    123

--- js/src/forum/components/LevelBar.js
+++ js/src/forum/components/LevelBar.js
@@ -1,5 +1,6 @@
 import Component from 'flarum/Component';
 import app from 'flarum/app';
+import username from 'flarum/helpers/username';
 
 export default class LevelBar extends Component {
     config() {
@@ -17,16 +18,18 @@ export default class LevelBar extends Component {
             expLevel = Math.floor(expTotal / 135),
             expPercent = (100 / 135) * ((expTotal) - (expLevel * 135));
 
-        return (
-            <div class="PostUser-level" title={`${expTotal} ${app.translator.trans('reflar-level-ranks.forum.desc.expText')}`}>
+        if (username !== app.translator.trans('core.lib.username.deleted_text')) {
+            return (
+                <div class="PostUser-level" title={`${expTotal} ${app.translator.trans('reflar-level-ranks.forum.desc.expText')}`}>
                 <span class="PostUser-text">
                     <span class="PostUser-levelText">{pointsText}</span>
                     &nbsp;
                     <span class="PostUser-levelPoints">{expLevel}</span>
                 </span>
-                <div class="PostUser-bar PostUser-bar--empty"></div>
-                <div class="PostUser-bar" style={"width: " + expPercent + '%;'}></div>
-            </div>
-        );
+                    <div class="PostUser-bar PostUser-bar--empty"></div>
+                    <div class="PostUser-bar" style={"width: " + expPercent + '%;'}></div>
+                </div>
+            );
+        }
     }
 }
